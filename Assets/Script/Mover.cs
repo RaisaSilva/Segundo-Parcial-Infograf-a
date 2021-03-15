7c76cb60e2f981457e88df32ae0e60bd7d8532c3
Raisa Alejnadra Silva Plata <raisa2201silva@gmail.com>
2021-03-15 17:08:10 -0400

    Proyecto 2do parcial

---
+++ Assets/Script/Mover.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+    private Vector2 movement;
+    public float speed;
+    public float jumpForce;
+
+
+//chocar con el piso
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius;
+//¿? esta en el piso
+    private bool isGrounded;
+
+
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        movement = new Vector2(horizontalInput, 0f);
+        if (horizontalInput < 0f)
+            {
+                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+            }
+            else if (horizontalInput > 0f)
+            {
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+            }
+
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        
+        float horizontalVelocity = movement.normalized.x * speed;
+        rigidbody.velocity = new Vector2(horizontalVelocity, rigidbody.velocity.y);
+
+    }
+    
+    void LateUpdate()
+    {
+           
+        animator.SetBool("Idle", movement == Vector2.zero);
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("VerticalVelocity", rigidbody.velocity.y);
+    }
+}
---
+++ Assets/Script/PlayerSelector.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelector : MonoBehaviour
+{
+    public GameObject[] changeCharacter; //Arreglo de previsualización de personajes con sus características
+    public GameObject[] characters; //Prefabs de los personajes que se crearán
+
+    private int index = 0;
+    
+    void Start()
+    {
+        //Todos los objetos de desactivan de pantalla
+        foreach(GameObject obj in changeCharacter)
+        {
+            obj.SetActive(false);
+        }
+        
+        
+        Select(index);
+    }
+
+    public void GoLeft()
+    {
+        changeCharacter[index].SetActive(false);
+        index--;
+        if(index < 0)
+        {
+            index = changeCharacter.Length - 1;
+        }
+        Select(index);
+    }
+
+    public void GoRight()
+    {
+        changeCharacter[index].SetActive(false);
+        index++;
+        if (index >= changeCharacter.Length)
+        {
+            index = 0;
+        }
+        Select(index);
+        
+    }
+
+    //El objecto que se selecciona aparece en pantalla
+    public void Select(int i)
+    {
+        foreach (GameObject obj in changeCharacter)
+        {
+            obj.SetActive(false); 
+        }
+        
+        changeCharacter[i].gameObject.SetActive(true);
+        PlayerStorage.playerPrefab = characters[i]; //Instancia el prefab que será creado para la escena principal
+    }
+}
